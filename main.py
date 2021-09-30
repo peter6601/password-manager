d236a0c33a02e9adfb5e2ec9ea0a98ce5c9809b9
Din Din <din.din@bigbear.me>
2021-09-30 12:31:38 +0800

    add to fetch ,update and save by json formatter

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import json
 from tkinter import *
 from tkinter import  messagebox
 import random
@@ -40,13 +41,28 @@ def save():
     website = website_entry.get()
     email = email_entry.get()
     password = password_entry.get()
+    new_data = {
+        website: {
+            "email": email,
+            "password": password
+        }
+    }
     if len(website) == 0 or len(password) == 0:
         messagebox.showinfo(title="Ooops", message="Please make sure you haven't ledt any field empty")
     else:
         is_ok = messagebox.askokcancel(title=website, message=f"These are the details entered: \nEmail: {email} \npassword: {password} \nIs is ok to save?")
         if is_ok:
-            with open("data.text", "a") as data_file:
-                data_file.write(f"{website} | {email} | {password}\n")
+            try:
+                with open("data.json", "r") as data_file:
+                    data = json.load(data_file)
+            except FileNotFoundError:
+                with open("data.json", "w") as data_file:
+                    json.dump(new_data,data_file, indent=4)
+            else:
+                data.update(new_data)
+                with open("data.json", "w") as data_file:
+                    json.dump(data, data_file, indent=4)
+            finally:
                 website_entry.delete(0,END)
                 password_entry.delete(0,END)
 
